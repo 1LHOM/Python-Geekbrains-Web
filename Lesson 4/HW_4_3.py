9e4e0e1655daf36ad02c598dd39e964bbed50329
1lhom <ilhom.winner@gmail.com>
2021-02-24 23:15:02 +0300

    Lesson 4 Homework 3, 5

---
+++ Lesson 4/HW_4_3.py
@@ -0,0 +1,39 @@
+# Homework 4_3
+import datetime
+import time
+
+from requests import get, utils
+
+
+def currency_rates(*args):
+
+    start_time = time.perf_counter()
+    print(f'{args} <---- this is what "args" received\n')
+    for char_code in args:
+        response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+        encodings = utils.get_encoding_from_headers(response.headers)
+        content = response.content.decode(encoding=encodings)
+        char_code = char_code.upper()
+        if char_code not in content:
+            return print(f'Ввлюта не найдена "{char_code}"')
+        else:
+            start_index = content.find(f'{char_code}')
+            data = content[(content.find('Date')+5):(content.find('name')-1)]
+            name_of_currency = content[(content.find('Name', start_index)+5):(content.find('</Name>', start_index))]
+            nominal = content[(content.find('Nominal', start_index)+8):(content.find('</Nominal>', start_index))]
+            rub, kop = content[(content.find('Value', start_index)+6):(content.find('</Value>', start_index))].split(',')
+            rub = int(rub)
+            kop = int(kop)
+            currency_rate = float(f'{rub}.{kop}')
+            print(f'Дата: {data}\nКурс валюты: {nominal} {name_of_currency}'
+                  f'\nВ рублях: {currency_rate}\n')
+    end_time = time.perf_counter()
+    end_process = datetime.datetime.now()
+    speed_of_process = (end_time - start_time)
+    print(f'Время завершения: {end_process}')
+    print(f'Запрос выполнен за: {speed_of_process} секунд')
+    return 0
+
+
+currency = ['EUR', 'USD', 'UZS']
+print(currency_rates(*currency))
